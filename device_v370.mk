d04ab8f1087efbaff81ab4688700ccb783cb7c34
Shreps <shr3ps@gmail.com>
2014-07-01 17:46:37 +0200

    v370 Kernel modules

--- device_v370.mk
+++ device_v370.mk
@@ -24,6 +24,7 @@ PRODUCT_COPY_FILES += \
 	$(MOD_SRC)/ccci_plat.ko:$(MOD_TGT)/ccci_plat.ko \
 	$(MOD_SRC)/devapc.ko:$(MOD_TGT)/devapc.ko \
 	$(MOD_SRC)/devinfo.ko:$(MOD_TGT)/devinfo.ko \
+	$(MOD_SRC)/hid-logitech-dj.ko:$(MOD_TGT)/hid-logitech-dj.ko \
 	$(MOD_SRC)/mtk_fm_drv.ko:$(MOD_TGT)/mtk_fm_drv.ko \
 	$(MOD_SRC)/mtk_hif_sdio.ko:$(MOD_TGT)/mtk_hif_sdio.ko \
 	$(MOD_SRC)/mtk_stp_bt.ko:$(MOD_TGT)/mtk_stp_bt.ko \
@@ -37,7 +38,8 @@ PRODUCT_COPY_FILES += \
 	$(MOD_SRC)/scsi_wait_scan.ko:$(MOD_TGT)/scsi_wait_scan.ko \
 	$(MOD_SRC)/sec.ko:$(MOD_TGT)/sec.ko \
 	$(MOD_SRC)/vcodec_kernel_driver.ko:$(MOD_TGT)/vcodec_kernel_driver.ko \
-	$(MOD_SRC)/wlan_mt6628.ko:$(MOD_TGT)/wlan_mt6628.ko
+	$(MOD_SRC)/wlan_mt6628.ko:$(MOD_TGT)/wlan_mt6628.ko \
+	$(MOD_SRC)/zram.ko:$(MOD_TGT)/zram.ko
 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/root/fstab.mt6589:root/fstab.mt6589
